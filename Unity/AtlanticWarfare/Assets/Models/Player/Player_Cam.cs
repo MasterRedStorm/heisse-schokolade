f0d777d129bbc8d59f91788d23d8668e8e59d0dc
Konny <konrad.kroeber@freenet.de>
2019-05-04 17:59:09 +0200

    finished Player Prefab

---
+++ Unity/AtlanticWarfare/Assets/Models/Player/Player_Cam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamera : MonoBehaviour
+{
+    [SerializeField] Transform Target = null;
+    public void Update()
+    {
+        if (Target == null) return;
+        transform.rotation = Quaternion.LookRotation(Target.position - transform.position, Vector3.up);
+    }
+}
