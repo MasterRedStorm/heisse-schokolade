3cbc8578f0df7af8bc416413050348ae808c2642
Lars Wobus <lars.wobus@web.de>
2019-05-05 12:50:28 +0200

    Add start, win and loose screen + app quit dialog

---
+++ Unity/AtlanticWarfare/Assets/Scripts/UI/TextColorBlinkAnimation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace  UI
+{
+    public class TextColorBlinkAnimation : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        [SerializeField] private Vector2 _alphaBounds = new Vector2(0.1f, 1.0f);
+
+        private Color _textColor;
+
+        private void Reset()
+        {
+            _text = GetComponent<TextMeshProUGUI>();
+        }
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _textColor = _text.color;
+            _textColor.a = _alphaBounds.x;
+            _text.color = _textColor;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _textColor.a = Mathf.PingPong(Time.time, 1) * _alphaBounds.y + _alphaBounds.x;
+            Debug.Log(_textColor.a);
+            _text.color = _textColor;
+        }
+    }
+}
